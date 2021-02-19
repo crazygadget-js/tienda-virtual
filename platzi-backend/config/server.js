26b4777ad529b5d037ee89f924e340679c9cff81
miguel chavez <angel960504@gmail.com>
2021-02-18 22:09:27 -0500

    actualizacion

--- platzi-backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '67fe4b613d9aba742a8264908bb7505d'),
-    },
-  },
-});
